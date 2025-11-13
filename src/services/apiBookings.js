b91962fa67742a9bd626c0bab89c263924c60393
Jonathan Udeh <jonathanudeh06@gmail.com>
2025-11-13 12:22:07 +0100

    Sorting bookings from the server side

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,13 +1,19 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings({ filter, sort }) {
+export async function getBookings({ filter, sortBy }) {
   let query = supabase
     .from("bookings")
     .select("*, cabins(name), guests(fullName, email)");
 
-  if (filter !== null)
-    query = query[filter.method || "eq"](filter.field, filter.value);
+  // Filter
+  if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
+
+  // Sort
+  if (sortBy)
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
 
   const { data, error } = await query;
   if (error) throw new Error("could not load Bookings");
