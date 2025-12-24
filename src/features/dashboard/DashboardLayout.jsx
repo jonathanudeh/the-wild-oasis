fa8edc3c5eb2beddd3969ab849f9958eab200b0e
Jonathan Udeh <jonathanudeh06@gmail.com>
2025-12-24 18:31:44 +0100

    Styling the dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
+import Stats from "./Stats";
+import { useCabins } from "../cabins/useCabins";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +11,32 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { isLoading, bookings } = useRecentBookings();
+  const {
+    isLoading: isLoading1,
+    stays,
+    confirmedStays,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isLoading: isLoading2 } = useCabins();
+
+  if (isLoading || isLoading1 || isLoading2) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
+      <div>Todays activities</div>
+      <div>Chart stay duration</div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
